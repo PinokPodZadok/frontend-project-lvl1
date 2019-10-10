7a852cd237aee56d34e168f7ad210cfafcc059c9
Nikolay Smirnov <itutk@mail.ru>
2019-10-10 09:17:14 +0500

    тренеровка по обнавлению кода

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
+
+console.log('Welcome');
 console.log('Welcome to the Brain Games');
+console.log('Welcome');
+
+
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+let readlineSync = require('readline-sync');
+
+// Wait for user's response.
+let userName = readlineSync.question('May I have your name? ');
+console.log('тестовое сообщение');
+console.log('Hello ' + userName + '!');
+console.log('тестовое сообщение');
