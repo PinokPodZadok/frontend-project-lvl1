aa3e4afe8c9820868a823cdbc3da8a81f95e4efa
Nikolay Smirnov <itutk@mail.ru>
2019-11-14 08:29:03 +0500

    добавил строчку

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
 console.log('');
 const actual = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${actual}!`);
