b076e0ec284bd4b7513400e5b46b51421f0dcb8e
Nikolay Smirnov <itutk@mail.ru>
2019-11-28 10:41:06 +0500

    add brainCalc

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 import { brainEven, brainGames } from '../index';
 
-brainGames();
 brainEven();
--- src/index.js
+++ src/index.js
@@ -2,23 +2,37 @@ import readlineSync from 'readline-sync';
 
 const brainGames = () => {
   console.log('Welcome to the Brain Games!');
+  
   const name = readlineSync.question('May I have your name? ');
+  
   console.log(`Hello, ${name}!`);
   console.log('');
 };
 
 const brainEven = () => {
+  
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name? ');
+  
+  console.log(`Hello, ${name}!`);
+  console.log('');
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   console.log('');
   console.log(`Hello, ${name}!`);
   console.log('');
+  
   const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-  const questionGame = () => {
+  
+  const evenGame = () => {
     const correctStr = () => console.log('Correct!');
     let win = 0;
+    
     while (win < 3) {
+      
       const num = getRandomInt(1, 999);
       console.log(`Question: ${num}`);
+      
       const ans = readlineSync.question('Your answer: ');
       if (num % 2 === 0 && ans === 'yes') {
         correctStr();
@@ -42,7 +56,57 @@ const brainEven = () => {
     }
     console.log(`Congratulations, ${name}!`);
   };
-  questionGame();
+  evenGame();
 };
 
-export { brainEven, brainGames };
+const brainCalc = () => {
+  console.log('Welcome to the Brain Games!');
+  
+  const name = readlineSync.question('May I have your name? ');
+  
+  console.log(`Hello, ${name}!`);
+  console.log('');
+  console.log('What is the result of the expression?');
+  console.log('');
+  
+  const brainCalcGame = () => {
+
+    const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+    
+    const intSymbol = getRandomInt(1,3);
+    const numFirst = getRandomInt(1, 999);
+    const numTwo = getRandomInt(1, 999);
+    
+    const result = (intSymbol, numFirst, numTwo) => {
+      if (intSymbol === 1) {
+        return numFirst + numTwo;
+      } else if (int === 2) {
+        return numFirst - numTwo;
+      } 
+      return numFirst * numTwo;
+    }
+    const correctStr = () => console.log('Correct!');
+   
+    
+    let win = 0;
+    while (win < 3) {
+      const questionGame = result(intSymbol, numFirst, numTwo);
+      console.log(`Question: ${questionGame}`);
+      
+      const ans = readlineSync.question('Your answer: ');
+      if (ans === questionGame) {
+        correctStr();
+        win += 1;
+      } else {
+        console.log(`'${ans}' is wrong answer ;(. Correct answer was '${questionGame}'.`);
+        console.log(`Let's try again, Sam!`);
+        brake;
+      }
+    }
+    console.log(`Congratulations, ${name}!`);
+
+  }
+    brainCalcGame();
+  };
+
+export { brainEven, brainGames, brainCalc };
