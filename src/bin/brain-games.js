c2409a60652a93e492199499ba18e3e8cac3752d
Nikolay Smirnov <itutk@mail.ru>
2019-12-11 19:57:39 +0500

    fix

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+// eslint-disable-next-line import/no-unresolved
 import { brainGames } from '../index';
 
+// eslint-disable-next-line no-undef
 brainGames();
--- src/games/brainGames.js
+++ src/games/brainGames.js
@@ -1,8 +1,8 @@
-/* import {} from '../index';
+/*import {} from '../index';
 // Функция игры brain-games.
 const brainGames = () => {
   // eslint-disable-next-line no-undef
   massegeWelcome('');
 };
 // eslint-disable-next-line import/prefer-default-export
-export { brainGames }; */
+export { brainGames };*/
